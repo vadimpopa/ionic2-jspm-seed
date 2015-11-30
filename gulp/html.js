c500fc116183cf7525082071ca10c0cd74a97f8f
Vadim Popa <vadimvpopa@gmail.com>
2015-11-30 12:19:00 +0200

    Removed templates

--- gulp/html.js
+++ gulp/html.js
@@ -2,7 +2,6 @@
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var angularTemplateCache = require('gulp-angular-templatecache');
 var concat = require('gulp-concat');
 
 // HTML livereload.
@@ -10,10 +9,3 @@ gulp.task('html', function() {
   gulp.src(global.paths.html)
     .pipe(connect.reload());
 });
-
-gulp.task('templates', function() {
-	gulp.src(global.paths.tpl)
-    .pipe(angularTemplateCache())
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('dist/'))
-})
